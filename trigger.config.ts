0cc5b0e58c61b3f1eba36e05d2b5826ed7635ef9
Djibrani Yuda <djibyuda@gmail.com>
2025-07-16 19:44:46 +0700

    fix the trigger config

--- trigger.config.ts
+++ trigger.config.ts
@@ -23,6 +23,8 @@ export default defineConfig({
   build: {
     extensions: [
       prismaExtension({
+        version: "5.20.0", // optional, we'll automatically detect the version if not provided
+        // update this to the path of your Prisma schema file
         schema: "prisma/schema.prisma",
       }),
     ],
