3bf1effffe2b800e50be687262d1f92c5cff793b
Djibrani Yuda <djibyuda@gmail.com>
2025-07-17 17:21:37 +0700

    fixing trigger config

--- trigger.config.ts
+++ trigger.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from "@trigger.dev/sdk/v3";
-import { prismaExtension } from "@trigger.dev/build/extensions/prisma";
 
 export default defineConfig({
   project: "proj_rvxecreedhorbvilkoza",
